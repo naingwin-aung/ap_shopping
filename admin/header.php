454e680b2c61854342aab2f5ad6bd5f2cf65fad3
naingwinaung <naingwinaung1710@gmail.com>
2020-09-08 09:48:02 +0630

    ap_shop module part 1

--- admin/header.php
+++ admin/header.php
@@ -15,9 +15,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <title>Ap Shopping</title>
 
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="/admin/plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="/admin/dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
@@ -68,7 +68,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="/admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Apshopping</span>
     </a>
@@ -77,11 +77,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
+  
         <div class="info">
-          <a href="#" class="d-block"> <?php echo $_SESSION['user_name']; ?></a>
+          <a href="#" class="d-block"> <?php echo ucfirst($_SESSION['user_name']); ?></a>
         </div>
       </div>
 
